7e0f5006f77fe877120916bd48ef0bbd04a3f41c
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-07-24 18:49:29 +0600

    add model path through config

--- app/config.py
+++ app/config.py
@@ -14,3 +14,14 @@ os.makedirs(IMAGE_DIR, exist_ok=True)
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 SERVER_DOMAIN = os.getenv("SERVER_DOMAIN", "https://photocards.saifullahmnsur.dev")
 ADMIN_SECRET_KEY = os.getenv("ADMIN_SECRET_KEY", "photocards_super_secret_key_2026")
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_DIR = os.path.join(BASE_DIR, "app", "media")
+WEIGHTS_DIR = os.path.join(BASE_DIR, "app", "weights")
+
+SERVER_DOMAIN = os.getenv("SERVER_DOMAIN", "https://photocards.saifullahmnsur.dev")
+
+os.makedirs(MEDIA_DIR, exist_ok=True)
+os.makedirs(WEIGHTS_DIR, exist_ok=True)
